dee51a318513b73b4753783d30c3f000b4bf6a62
Benjamin White <benjamin.e.white1@gmail.com>
2017-01-18 14:24:58 -0500

    Revert "Set NODE_ENV for tests.."
    This reverts commit 5a78172bce6b08ec7e0dd6f85e2d0b0a9ff0517f.

--- db/sequelize.js
+++ db/sequelize.js
@@ -14,6 +14,7 @@ const Sequelize = require('sequelize');
 // http://docs.sequelizejs.com/en/latest/api/sequelize/#new-sequelizedatabase-usernamenull-passwordnull-options
 const {DATABASE_URL, SEQUELIZE_OPTIONS} = require('../config.js');
 
+console.log(`Connecting to database at ${DATABASE_URL}`);
 const sequelize = new Sequelize(DATABASE_URL, SEQUELIZE_OPTIONS);
 
 // we shouldn't initialize a new Sequelize instance anywhere else

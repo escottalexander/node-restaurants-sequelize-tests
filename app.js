17e825861c7cc1b4d748fcfa5e2e528caaf3ced5
Benjamin White <benjamin.e.white1@gmail.com>
2017-01-18 13:08:37 -0500

    remove grades endpoint for starter

--- app.js
+++ app.js
@@ -8,7 +8,6 @@ const express = require('express');
 const morgan = require('morgan');
 
 const restaurantsRouter = require('./routes/restaurants');
-const gradesRouter = require('./routes/grades');
 
 
 // Set up the express app
@@ -18,7 +17,6 @@ app.use(morgan('common'));
 app.use(bodyParser.json());
 
 app.use('/restaurants', restaurantsRouter);
-app.use('/grades', gradesRouter);
 
 app.use('*', function(req, res) {
   res.status(404).json({message: 'Not Found'});
